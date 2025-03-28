69293eab0f89e6d578f49644d402b351ee2283b4
Johann Wagner <johann.wagner@wobcom.de>
2025-03-28 13:58:16 +0100

    Added logging

--- sparky_web/settings.py
+++ sparky_web/settings.py
@@ -115,6 +115,21 @@ if LDAP_CONFIGURED:
 
 VERSION = '1.5.0'
 
+LOG_LEVEL = getattr(configuration, "DEBUG")
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": LOG_LEVEL,
+    },
+}
+
 # Application definition
 
 INSTALLED_APPS = [
